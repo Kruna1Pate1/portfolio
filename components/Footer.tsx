d1ea94ace28350da4139624481446d6f838fe328
Krunal Patel <kbpatel5802@gmail.com>
2022-09-29 15:47:09 +0530

    Title and Footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer className="w-full h-16 bg-[#0d1117] z-40">
       <div className="flex justify-center items-center w-full h-full px-3 md:px-16">
         <p className="text-gray-300">
-          &copy; Designed &amp; Developed by
+          Made with <span className="text-[#e25555] text-2xl"> &#9829; </span>
+          by
           <span className="text-white"> Krunal Patel </span>
         </p>
       </div>
